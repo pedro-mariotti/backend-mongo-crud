3e759d394e0df5fa9b6ed35c8cb0bfe34d87ea44
Pedro Mariotti <mariottipedro14@gmail.com>
2025-04-30 11:24:22 -0300

    Commit

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    console.log("Registering user", req.body);
+  console.log("Registering user", req.body);
+  res.status(201).json({ message: "User registered successfully" });
 });
 
 export default router;
